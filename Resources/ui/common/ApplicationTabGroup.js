120ad6e0e4b323cad866496731d5179f49b03650
harryfeng <harry@littleroom.ca>
2013-03-15 17:41:02 -0400

    show 4 tab from tab array

--- Resources/ui/common/ApplicationTabGroup.js
+++ Resources/ui/common/ApplicationTabGroup.js
@@ -8,30 +8,21 @@ function ApplicationTabGroup(Window) {
 		win3 = new Window[2](L('settings'));
 		win4 = new Window[3](L('settings'));
 	var winArray = [win1,win2,win3,win4];
-	var tabArrauy=[];
-	for(var value in winArray)
+	var tabArray=[];
+	for(var i=0;i<winArray.length;i++)
 	{
 		var tab = Ti.UI.createTab({
-		window: value
+		window: winArray[i],
+		title:i
 		});
-		value.containingTab = tab;
-		tabArrauy.push(tab)
+		winArray[i].containingTab = tab;
+		tabArray.push(tab)
+	}
+	for(var k=0;k<tabArray.length;k++)
+	{
+		self.addTab(tabArray[k]);
 	}
 	
-	var tab1 = Ti.UI.createTab({
-		window: win1
-	});
-	win1.containingTab = tab1;
-	
-	var tab2 = Ti.UI.createTab({
-		title: L('settings'),
-		icon: '/images/KS_nav_views.png',
-		window: win2
-	});
-	win2.containingTab = tab2;
-	
-	self.addTab(tab1);
-	self.addTab(tab2);
 	
 	return self;
 };
