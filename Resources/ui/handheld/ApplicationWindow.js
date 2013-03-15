b07ef46081f6b26b6fb14e90dd72a8682019cc7c
harryfeng <harry@littleroom.ca>
2013-03-15 19:04:32 -0400

    read image height and width, set propery dimension for the bottom tab background image

--- Resources/ui/handheld/ApplicationWindow.js
+++ Resources/ui/handheld/ApplicationWindow.js
@@ -11,14 +11,18 @@ function ApplicationWindow(title) {
 		height:50,
 		top:0
 		});
-		
 	
+	imageFile = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, "/images/iphoneImage/bartenderBottomTab.png");
+	var image = imageFile.read();
+	var imageHeight = image.height;
+	var imageWidth = image.width;
+	Ti.API.log(imageWidth)
 	for(var i=0;i<4;i++)
 	{
 		var bottomTabView = Titanium.UI.createView({
 		backgroundImage:'/images/iphoneImage/bartenderBottomTab.png',
 		width:bottomTabWidth,
-		height:50,
+		height:imageHeight*bottomTabWidth/imageWidth,
 		bottom:0,
 		left:0+bottomTabWidth*i,
 		});
