23af74ee0148c0ba5ad8a4fc86c8f9aec99ddeba
harryfeng <harry@littleroom.ca>
2013-03-20 10:51:56 -0400

    add addBackButtonFunction

--- Resources/ui/common/common.js
+++ Resources/ui/common/common.js
@@ -1,10 +1,22 @@
 var platformWidth = Titanium.Platform.displayCaps.platformWidth;
 var platformHeight = Titanium.Platform.displayCaps.platformHeight;
 
-var getImageByFileName = function(fileName){
+var getImageByFileName = function(fileName) {
 	imageFile = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, fileName);
 	var image = imageFile.read();
 	return image;
 }
+var addBackButton = function(targetWindow, eventCallBack) {
+	var button = Titanium.UI.createButton({
+		height : getImageByFileName("/images/iphoneImage/backButton.png").height / 2,
+		width : getImageByFileName("/images/iphoneImage/backButton.png").width / 2,
+		backgroundImage : "/images/iphoneImage/backButton.png",
+	});
+	targetWindow.leftNavButton = button;
 
+	button.addEventListener('click', function() {
+		eventCallBack();
+	});
 
+	return button;
+}
--- Resources/ui/handheld/bartendersWindow.js
+++ Resources/ui/handheld/bartendersWindow.js
@@ -33,18 +33,7 @@ function bartendersWindow(commonProperties) {
 			self.remove(scrollView);
 			var win = new commonWindow();
 
-			var button = Titanium.UI.createButton({
-				height : getImageByFileName("/images/iphoneImage/backButton.png").height / 2,
-				width : getImageByFileName("/images/iphoneImage/backButton.png").width / 2,
-				backgroundImage : "/images/iphoneImage/backButton.png",
-			});
-			self.leftNavButton = button;
-			var winview = Ti.UI.createView({
-				backgroundImage : '/images/iphoneImage/bartenderImage.png',
-				height : getImageByFileName('/images/iphoneImage/bartenderImage.png').height * platformWidth / getImageByFileName('/images/iphoneImage/bartenderImage.png').width,
-			});
-
-			button.addEventListener('click', function() {
+			var button = addBackButton(self, function() {
 				self.add(scrollView);
 				self.leftNavButton = null;
 				scrollViewInside.animate({
@@ -53,13 +42,15 @@ function bartendersWindow(commonProperties) {
 				}, function() {
 					self.remove(scrollViewInside);
 				});
-			})
+			});
+			var winview = Ti.UI.createView({
+				backgroundImage : '/images/iphoneImage/bartenderImage.png',
+				height : getImageByFileName('/images/iphoneImage/bartenderImage.png').height * platformWidth / getImageByFileName('/images/iphoneImage/bartenderImage.png').width,
+			});
 			var scrollViewInside = new createView();
-			//	scrollViewInside.top =scrollViewInside.top-50;
 			scrollViewInside.height = scrollViewInside.height;
 			scrollViewInside.zIndex = 9;
 			scrollViewInside.add(winview)
-			//winview.add(button);
 			self.add(scrollViewInside);
 		})
 		scrollView.add(view);
