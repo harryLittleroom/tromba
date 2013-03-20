35069b9042f99baa6cdad7704a1afa1a253da46b
harryfeng <harry@littleroom.ca>
2013-03-20 10:59:07 -0400

    create 3 button using constructor

--- Resources/ui/handheld/storyWindow.js
+++ Resources/ui/handheld/storyWindow.js
@@ -2,35 +2,20 @@ var commonWindow = require('ui/handheld/ApplicationWindow');
 
 function storyWindow(commonProperties) {
 	var self = new commonWindow();
-	 
-	
-	
-	var storyButton = Titanium.UI.createButton({
-		borderRadius:10,
-		//backgroundColor :'#003569',
-		backgroundImage: '/images/iphoneImage/storyButton.png',
-		height:getImageByFileName('/images/iphoneImage/storyButton.png').height/2,
-		width:getImageByFileName('/images/iphoneImage/tequilaButton.png').width/2,
-		top : 10,
-	});
-	
-	var whyButton = Titanium.UI.createButton({
-		borderRadius:10,
-		//backgroundColor :'#003569',
-		backgroundImage: '/images/iphoneImage/whyUseButton.png',
-		height:getImageByFileName('/images/iphoneImage/tequilaButton.png').height/2,
-		width:getImageByFileName('/images/iphoneImage/tequilaButton.png').width/2,
-		top : 10+getImageByFileName('/images/iphoneImage/tequilaButton.png').height/2+10,
-	});
-	
-	var tequilaButton = Titanium.UI.createButton({
-		borderRadius:10,
-		//backgroundColor :'#003569',
-		backgroundImage: '/images/iphoneImage/tequilaButton.png',
-		height:getImageByFileName('/images/iphoneImage/tequilaButton.png').height/2,
-		width:getImageByFileName('/images/iphoneImage/tequilaButton.png').width/2,
-		top : 10+getImageByFileName('/images/iphoneImage/tequilaButton.png').height+20,
-	});
+
+	var createStoryButton = function(backGroundImage, top) {
+		var button = Titanium.UI.createButton({
+			borderRadius : 10,
+			backgroundImage : backGroundImage,
+			height : getImageByFileName(backGroundImage).height / 2,
+			width : getImageByFileName(backGroundImage).width / 2,
+			top : top,
+		});
+		return button
+	}
+	var storyButton = new createStoryButton('/images/iphoneImage/storyButton.png', 10);
+	var whyButton = new createStoryButton('/images/iphoneImage/whyUseButton.png', 10 + getImageByFileName('/images/iphoneImage/tequilaButton.png').height / 2 + 10);
+	var tequilaButton = new createStoryButton('/images/iphoneImage/tequilaButton.png', 10 + getImageByFileName('/images/iphoneImage/tequilaButton.png').height + 20);
 	self.add(storyButton);
 	self.add(whyButton);
 	self.add(tequilaButton);
