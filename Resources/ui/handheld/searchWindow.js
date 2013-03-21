5e8c422190ecabc3f4165c340ae32049bb9d0c02
harryfeng <harry@littleroom.ca>
2013-03-21 10:35:57 -0400

    add a text field in search window

--- Resources/ui/handheld/searchWindow.js
+++ Resources/ui/handheld/searchWindow.js
@@ -3,7 +3,21 @@ var commonWindow = require('ui/handheld/ApplicationWindow');
 function searchWindow(commonProperties) {
 	var self = new commonWindow();
 	
-
+	var textBox = Ti.UI.createTextField({
+		width:platformWidth*0.8,
+		height:30,
+		top:20,
+		borderRadius:10,
+		hintText:'Enter Keywords',
+		backgroundColor:'white',
+		paddingLeft:10,
+		autocorrect:false,
+		clearButtonMode: Titanium.UI.INPUT_BUTTONMODE_ONFOCUS
+	});
+	
+	//textBox.clearButtonMode = UITextFieldViewModeWhileEditing;
+	
+	self.add(textBox);
 	
 	return self;
 };
